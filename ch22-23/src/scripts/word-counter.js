8acd38ae95514b7e35b4f4b85895dda78efc00ef
PerJakobNilsson <jakob.nilsson@tidaholm.se>
2025-12-26 12:58:09 +0100

    did ch23

--- ch22-23/src/scripts/word-counter.js
+++ ch22-23/src/scripts/word-counter.js
@@ -1,13 +1,21 @@
-function insertReadingTime() {
-    let $body = document.body;
-
-let text = $body.innerText;
-let list = text.split(" ");
-let words = list.length;
-
-let duration = words / 250;
-$body.innerHTML= `<p>(Reading time: ${duration} minutes)</p>
-${$body.innerHTML}`;
+function countWords(the_text) {
+  return the_text.split(" ").length;
+}
+function wordsToMinutes(words) {
+  return words / 250;
 }
 
-insertReadingTime();
+function insertReadingTime($p) {
+  if ($p.innerText && $p.matches("p")) {
+    let words = countWords($p.innerText);
+    let duration = wordsToMinutes(words);
+    $p.innerText += " \n(Reading time: " + duration + " minutes).";
+  } else {
+    console.error("Cannot parse text.");
+  }
+}
+$paragraphs = document.querySelectorAll("p");
+let count = $paragraphs.length;
+for (let i = 0; i < count; i++) {
+  insertReadingTime($paragraphs[i]);
+}
